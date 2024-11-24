a90cd4256690dce62f048b6cffa1e28606ef42fb
NoobTenLuka <noobtenluka@gmail.com>
2024-11-24 03:21:47 +0100

    finish parser rewrite and start writing pretty printer

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,8 @@
+use std::io::{self, BufWriter, Stdout, Write};
+
 use crate::{RegexCompileError, Token};
 
-struct RegexNode {
+struct RegexAst {
     has_hat: bool,
     has_dollar: bool,
     body: Body
@@ -24,14 +26,17 @@ enum ExpressionType {
     ZeroOrMore,
     OneOrMore,
     ZeroOrOne,
-    AmountQuantifier {
-        min: u64,
-        max: Option<u64>
-    },
+    AmountQuantifier(AmountQuantifier)
+}
+
+enum AmountQuantifier {
+    Min(u64),
+    MinOrMore(u64),
+    MinMax(u64, u64)
 }
 
 enum Atom {
-    Symbol(char),
+    Symbol(Symbol),
     Group(Body),
     Range {
         inverted: bool,
@@ -40,20 +45,25 @@ enum Atom {
 }
 
 enum Range {
-    Symbol(char),
+    Symbol(Symbol),
     FromTo {
         from: char,
         to: char
     }
 }
 
-struct LL1Parser<I: Iterator<Item = Token>> {
+enum Symbol {
+    EscapeGroup(char),
+    Literal(char)
+}
+
+pub struct LL1Parser<I: Iterator<Item = Token>> {
     tokens: I,
     current_token: Token
 }
 
 impl<I: Iterator<Item = Token>> LL1Parser<I> {
-    fn new(mut tokens: I) -> Result<Self, RegexCompileError> {
+    pub fn new(mut tokens: I) -> Result<Self, RegexCompileError> {
         let first_token = tokens.next().ok_or(RegexCompileError::EmptyError)?;
 
         Ok(Self {
@@ -87,7 +97,7 @@ impl<I: Iterator<Item = Token>> LL1Parser<I> {
     }
 
     // regex ::= '^'? body '$'?
-    fn parse(&mut self) -> Result<RegexNode, RegexCompileError> {
+    pub fn parse(&mut self) -> Result<RegexAst, RegexCompileError> {
         let has_hat = if matches!(self.current_token, Token::Hat) {
             self.accept_it()?;
             true
@@ -104,7 +114,7 @@ impl<I: Iterator<Item = Token>> LL1Parser<I> {
             false
         };
 
-        Ok(RegexNode {
+        Ok(RegexAst {
             has_hat, body, has_dollar 
         })
     }
@@ -159,11 +169,11 @@ impl<I: Iterator<Item = Token>> LL1Parser<I> {
             Token::OpeningAmountBracket => {
                 self.accept_it()?;
 
-                let (min, max) = self.parse_amount_expression()?;
+                let amount_quantifier = self.parse_amount_expression()?;
 
                 self.accept(Token::ClosingAmountBracket)?;
 
-                ExpressionType::AmountQuantifier { min, max }
+                ExpressionType::AmountQuantifier(amount_quantifier)
             },
             _ => ExpressionType::Single
         };
@@ -174,10 +184,8 @@ impl<I: Iterator<Item = Token>> LL1Parser<I> {
     // atom ::= symbol | '(' body ')' | '[' rangeExpr ']'
     fn parse_atom(&mut self) -> Result<Atom, RegexCompileError> {
         Ok(match self.current_token {
-            Token::Escape => todo!(), //TODO: Deal with escapes properly
-            Token::Literal(x) => {
-                self.accept_it()?;
-                Atom::Symbol(x)
+            Token::Escape | Token::Literal(_) => {
+                Atom::Symbol(self.parse_symbol()?)
             }
             Token::OpeningBracket => {
                 self.accept_it()?;
@@ -202,30 +210,33 @@ impl<I: Iterator<Item = Token>> LL1Parser<I> {
     }
 
     // amountExpr ::= number (',' number? )?
-    fn parse_amount_expression(&mut self) -> Result<(u64, Option<u64>), RegexCompileError> {
+    fn parse_amount_expression(&mut self) -> Result<AmountQuantifier, RegexCompileError> {
         let min = self.parse_num()?;
 
-        let max = if self.current_token == Token::Comma {
+        if self.current_token == Token::Comma {
             self.accept_it()?;
 
-            Some(self.parse_num()?)
-        } else { None };
+            if self.current_token == Token::ClosingAmountBracket {
+                return Ok(AmountQuantifier::MinOrMore(min))
+            }
 
-        Ok((min, max))
+            return Ok(AmountQuantifier::MinMax(min, self.parse_num()?))
+        }
+
+        Ok(AmountQuantifier::Min(min))
     }
 
     // number ::= literal+
     fn parse_num(&mut self) -> Result<u64, RegexCompileError> {
-        let mut num = 0;
+        let mut number_string = String::new();
+        number_string.push(self.accept_lit()?);
+
         while let Token::Literal(c) = self.current_token {
             self.accept_it()?;
-            num = num * 10
-                + c.to_digit(10)
-                    .ok_or(RegexCompileError::UnexpectedSymbolError(c))? as u64;
-            //TODO: check if u64 exceeded
+            number_string.push(c);
         };
 
-        Ok(num)
+        Ok(u64::from_str_radix(&number_string, 10).map_err(|_| RegexCompileError::EmptyError)?) // TODO: Replace empty error with better errors depending of too big, or invalid chars
     }
 
     // rangeExpr ::= '^'? ( symbol ('-' symbol)? )+
@@ -235,10 +246,135 @@ impl<I: Iterator<Item = Token>> LL1Parser<I> {
             true
         } else { false };
 
-        let ranges = Vec::new();
+        let mut ranges = Vec::new();
 
-        
+        ranges.push(self.parse_inner_range()?);
+
+        while self.current_token != Token::ClosingBracket {
+            ranges.push(self.parse_inner_range()?);
+        }
 
         Ok((inverted, ranges))
+    }
+
+    // symbol ('-' literal)?
+    fn parse_inner_range(&mut self) -> Result<Range, RegexCompileError> {
+        let start = self.parse_symbol()?;
+
+        if self.current_token == Token::Minus {
+            let start = if let Symbol::Literal(x) = start {
+                x
+            } else {
+                return Err(RegexCompileError::EmptyError) // TODO: make an error like unallowed
+                                                          // escape group error
+            };
+
+            self.accept_it();
+            let end = self.accept_lit()?;
+
+            return Ok(Range::FromTo { from: start, to: end })
+        } 
+
+        return Ok(Range::Symbol(start))
+    }
+
+    // literal | '\'.
+    fn parse_symbol(&mut self) -> Result<Symbol, RegexCompileError> {
+        if let Token::Literal(x) = self.current_token {
+            self.accept_it()?;
+            return Ok(Symbol::Literal(x));
+        }
+
+        self.accept(Token::Escape)?;
+
+        return Ok(Symbol::Literal(match self.current_token {
+            Token::Star => '*',
+            Token::Plus => '+',
+            Token::Dot => '.',
+            Token::OpeningBracket => '[',
+            Token::ClosingBracket => ']',
+            Token::OpeningAmountBracket => '{',
+            Token::ClosingAmountBracket => '}',
+            Token::OpeningParentheses => '(',
+            Token::ClosingParantheses => ')',
+            Token::Escape => '\\',
+            Token::QuestionMark => '?',
+            Token::Dollar => '$',
+            Token::Hat => '^',
+            Token::Minus => '-',
+            Token::Bar => '|',
+            Token::Comma => ',',
+            Token::EOL => {
+                return Err(RegexCompileError::EmptyError)
+            },
+            Token::Literal(x) => {
+                return Ok(Symbol::EscapeGroup(x))
+            }
+        }))
+    }
+}
+
+struct PrettyPrinter<T: Write> {
+    buf: BufWriter<T>
+}
+
+impl<T: Write> PrettyPrinter<T> {
+    pub fn pretty_print_ast(&mut self, ast: RegexAst, original: &str) {
+        writeln!(&mut self.buf, "Abstract Syntax Tree for {}", original);
+        writeln!(&mut self.buf, "Starts with Hat: {}", ast.has_hat);
+        writeln!(&mut self.buf, "Ends with Dollar: {}", ast.has_dollar);
+        self.pp_body(ast.body, 0);
+    }
+
+    fn pp_body(&mut self, body: Body, indentation_level: usize) {
+        write!(&mut self.buf, "{}", "| ".repeat(indentation_level));
+        writeln!(&mut self.buf, "Body:");
+        for disjunctive in body.disjunctives {
+            self.pp_disjunctive(disjunctive, indentation_level + 1);
+        }
+    }
+
+    fn pp_disjunctive(&mut self, disjunctive: Expressions, indentation_level: usize) {
+        write!(&mut self.buf, "{}", "| ".repeat(indentation_level));
+        writeln!(&mut self.buf, "Expressions:");
+        for expression in disjunctive.expressions {
+            self.pp_expression(expression, indentation_level + 1);
+        }
+    }
+
+    fn pp_expression(&mut self, expr: Expression, indentation_level: usize) {
+        write!(&mut self.buf, "{}", "| ".repeat(indentation_level));
+        match expr.expr_type {
+            ExpressionType::Single => {
+                writeln!(&mut self.buf, "Single:");
+            },
+            ExpressionType::ZeroOrMore => {
+                writeln!(&mut self.buf, "ZeroOrMore (*):");
+            },
+            ExpressionType::OneOrMore => {
+                writeln!(&mut self.buf, "OneOrMore (+):");
+            },
+            ExpressionType::ZeroOrOne => {
+                writeln!(&mut self.buf, "ZeroOrOne (?):");
+            },
+            ExpressionType::AmountQuantifier(amount_quantifier) => {
+                match amount_quantifier {
+                    AmountQuantifier::Min(min) => {
+                        writeln!(&mut self.buf, "AmountQuantifier ({}):", min);
+                    },
+                    AmountQuantifier::MinOrMore(min) => {
+                        writeln!(&mut self.buf, "AmountQuantifier ({},):", min);
+                    },
+                    AmountQuantifier::MinMax(min, max) => {
+                        writeln!(&mut self.buf, "AmountQuantifier ({},{}):", min, max);
+                    },
+                }
+            },
+        }
+        self.pp_atom(expr.atom, indentation_level + 1);
+    }
+
+    fn pp_atom(&mut self, atom: Atom, indentation_level: usize) {
+        
     }
 }
